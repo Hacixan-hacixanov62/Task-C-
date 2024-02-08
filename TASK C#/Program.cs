d8c44514396edcb7da19727829206e94227aecdb
unknown <hajikhanih@code.edu.az>
2024-02-07 20:57:32 -0800

    Task

---
+++ TASK C#/Program.cs
@@ -0,0 +1,59 @@
+﻿//#homeworks
+
+//int n = 35;
+// if  ( n%3 ==0 && n%7==0)
+//{
+//    Console.WriteLine( "divide" );
+//}
+//else 
+//{
+//    Console.WriteLine("not divide");
+//}
+
+
+
+//int n = 20;
+//int m = 22;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//    Console.WriteLine( "yes");
+//    int sum = m + n;
+
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("not");
+//}
+
+
+
+
+//int n = 1;
+//int m = 20;
+//int sum = n + m;
+//for (int i = n; i < m; i++)
+//{
+//    if (i%2==1)
+//    {
+//        sum += (int)i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//int a = 1;
+//int b = 100;
+
+//for (int i =1; i<=100; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+        
+//    }
+//}
+//Console.WriteLine(i);
